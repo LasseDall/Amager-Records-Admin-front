2a1a8d211adc3ce1458a7a46e4d02fc59dc7a2f0
LasseDall <lass794rrstud.kea.dk>
2023-05-19 11:21:04 +0200

    tek commit

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
-export const API_URL = "amager-records.azurewebsites.net/api"
+export const API_URL = "https://amager-records.azurewebsites.net/api"
 
 export const FETCH_NO_API_ERROR = " (Is the API online or did the endpoint exists ?)"
 
